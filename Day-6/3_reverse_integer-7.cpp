76d4bc989a2dec2dd0a5ce1d139a56f0cf9b06fe
Shubham  Bhade <121212493+shubhambhade@users.noreply.github.com>
2024-11-03 16:08:43 +0530

    Add files via upload

---
+++ Day-6/3_reverse_integer-7.cpp
@@ -0,0 +1,62 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-2^31 <= x <= 2^31 - 1
+*/
+
+#include<iostream>
+using namespace std;
+
+class ReverseInteger 
+{
+public:
+    int reverse(int x) {
+        int ans = 0 , remainder;
+
+        while(x != 0)
+        {
+            remainder = x % 10;
+
+            x /= 10;
+
+            if(ans > INT_MAX/10 || ans<INT_MIN/10)
+            return 0;
+
+            ans = ans * 10 + remainder;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int number;
+    cout<<"Enter Number : "<<endl;
+    cin>>number;
+
+    ReverseInteger ri;
+    int answer = ri.reverse(number);
+    cout<<answer;
+    
+return 0;
+}
