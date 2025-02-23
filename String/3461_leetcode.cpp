0ef369c9ceb5a7024f9573d93909e55e3e57913e
Shubham  Bhade <121212493+shubhambhade@users.noreply.github.com>
2025-02-23 09:14:20 +0000

    3461_leetcode

---
+++ String/3461_leetcode.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        bool hasSameDigits(string s) {
+            
+            while(s.length()>2)
+            {
+                string str="";
+                for(int i = 0; i < s.length()-1; i++)
+                {
+                    str += to_string((s[i]-'0' + s[i+1]-'0')%10);
+                }
+                s=str;
+            }
+            return s[0] == s[1];
+      }
+ };
